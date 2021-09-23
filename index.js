6e7b2352b8b3072a2481c61cc40f1ee5539cbfe1
Justin Chittarath <justinchitt@gmail.com>
2021-09-22 21:03:33 -0600

    commit tree 0451e5d08e261a416874bd2eb74c839fb0b5d491

--- index.js
+++ index.js
@@ -12,5 +12,50 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  let newOne = tutorials.map(function whatever(element){
+    let arrayOfWords = element.split(" ");
+    let innerArray = arrayOfWords.map(function(elements){
+      let capitals = elements[0].toUpperCase() + elements.slice(1)
+      // let combined = capitals + elements.slice(1)
+      return capitals;
+    })
+    let final =innerArray.join(' ')
+    console.log(final)
+    return final;
+  
+  })
+  return newOne
 }
+console.log(titleCased())
+
+
+
+
+// let test = tutorials[2].split(" ")[0][0].toUpperCase()
+
+// function capitalize(array) {
+//   let what = array[0].split(' ')
+//   let who = what[1]
+//   let where = who
+//   let the = who.charAt(0).toUpperCase()
+//   let word = who.substr(1)
+//   let final = the + word
+//   return final
+// }
+
+// console.log(titleCased());
+
+// function whatever(element){
+//   let arrayOfWords = element.split(" ");
+//   let innerArray = arrayOfWords.map(function(elements){
+//     let capitals = elements[0].toUpperCase()
+//     let combined = capitals + elements.slice(1)
+//     // console.log(combined)
+//     return combined;
+//   })
+//   let final =innerArray.join(' ')
+//   // console.log(final)
+//   return [final];
+
+// }
+// console.log(whatever(tutorials[0]))
